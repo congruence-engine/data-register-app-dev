50a77d25afdd5addd8a51569ee571accc522c380
Felix Needham-Simpson <felixnesi6@gmail.com>
2024-10-08 14:48:19 +0100

    Created docs folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import Search from "./components/Search";
+import { Suspense } from "react";
 
 const Home = () => {
   return (
     <>
-        <h1 className='home'>Search</h1>
+      <h1 className='home'>Search</h1>
+      <Suspense>
         <Search />
+      </Suspense>
     </>
   );
 }
