2131cb01c5e3d2cfd1726c8ed93f3ea5e26b0557
Felix Needham-Simpson <felixnesi6@gmail.com>
2024-11-29 12:33:45 +0000

    Added additional info into footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,9 +3,16 @@
 const Footer = () => {
     return (
         <footer>
-            <div className="container">
-                <p>Footer goes here</p>
-            </div>
+            <div className="footer-container">
+                <div className='footer-information'>
+                    <p>This work is licensed under a <a href='https://creativecommons.org/licenses/by/4.0/'>Creative Commons Attribution 4.0 License - CC BY 4.0</a>.</p>
+                    <a href='https://congruence-engine.wikibase.cloud/wiki/Main_Page'>About Congruence Engine Wikibase</a>
+                </div>
+                <div className='footer-credits'>
+                    <p>Developers: Kunika Kono, Felix Needham-Simpson.</p>
+                    <p>Lead: Felix Needham-Simpson</p>
+                </div>
+            </div>          
         </footer>
     );
 }
